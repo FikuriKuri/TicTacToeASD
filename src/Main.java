e8846978c0861601215851c6ed6579dabb1b2269
FikuriKuri <fikrimuhammad294@gmail.com>
2024-11-28 10:34:39 +0700

    Second Commit: Update on Name

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-<<<<<<< HEAD
-=======
 /**
  * ES234317-Algorithm and Data Structures
  * Semester Ganjil, 2024/2025
  * Group Capstone Project
  * Group #1
- * 1 - 5026231198 - Muhammad Fikri Khalilullah
+ * 1 - Student ID - Student Name 1
  * 2 - Student ID - Student Name 2
  * 3 - Student ID - Student Name 3
  */
-
->>>>>>> d1ca902 (Second Commit: Update on Name)
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
